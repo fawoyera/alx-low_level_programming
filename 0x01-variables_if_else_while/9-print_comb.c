b4944ec59544e2d844208c93044da5005477cb84
fawoyera <femiawoyera@gmail.com>
2022-10-15 04:23:42 -0700

    print the digits 0 to 9 separated by comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,14 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
 		if (i < 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar(i + '0');
-		}
+	}
+	putchar('\n');
 	return (0);
 }
