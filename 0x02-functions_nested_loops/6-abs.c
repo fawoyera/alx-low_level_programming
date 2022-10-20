b09f32e9cd702464e5c622873b7eac9850a58978
fawoyera <femiawoyera@gmail.com>
2022-10-20 08:14:41 -0700

    print absolute value of input integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an input integer
+ * @n: the integer number
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
