b66bacdeb04c868725f62585b0f018feb422db4f
fawoyera <femiawoyera@gmail.com>
2022-10-20 15:19:32 -0700

    print the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - print the sum of two integers
+ * @i: the first integer
+ * @j: the second integer
+ * Return: Always 0
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
