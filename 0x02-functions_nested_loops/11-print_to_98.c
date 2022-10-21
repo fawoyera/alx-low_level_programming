429f0d64d72358cf9f8c73268d237b1ae840b8ca
fawoyera <femiawoyera@gmail.com>
2022-10-20 17:34:00 -0700

    print number up till 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - print numbers up to 98
+ * print_to_98 - print from a number to 98
  * @n: the starting number
  * Return: Always 0
  */
@@ -13,44 +14,9 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
-			if (i >= 0)
-			{
-				if (i < 10)
-				{
-					_putchar(i + '0');
-				}
-				else
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				int j = 0 - i;
-
-				_putchar('-');
-				if (j >= 100)
-				{
-					_putchar((j / 100) + '0');
-					_putchar(((j % 100) / 10) + '0');
-					_putchar((j % 10) + '0');
-				}
-				else if ((j >= 10) && (j <= 99))
-				{
-					_putchar((j / 10) + '0');
-					_putchar((j % 10) + '0');
-				}
-				if (j < 10)
-					_putchar(j + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
 	else
@@ -59,16 +25,10 @@ void print_to_98(int n)
 
 		for (i = n; i >= 98; i--)
 		{
-			if (i > 99)
-				_putchar((i / 100) + '0');
-			_putchar(((i % 100) / 10) + '0');
-			_putchar((i % 10) + '0');
+			printf("%i", i);
 			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf(", ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
