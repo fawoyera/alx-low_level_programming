36be5ab554a30b52c389321ca29bc8b2567d73ec
fawoyera <femiawoyera@gmail.com>
2022-10-27 13:18:45 -0700

    concatenate two strings with n characters from src string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * strncat - concatenate two strings using n characters from src string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters of src to concatenate
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+	{
+		if (j < n)
+		{
+			*(dest + i) = *(src + j);
+			j++;
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (j < n)
+		*(dest + i) = '\0';
+	return (dest);
+}
