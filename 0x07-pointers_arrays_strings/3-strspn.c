2a1087e9db89db8c8b1e584d9d351407ec6ab03c
fawoyera <femiawoyera@gmail.com>
2023-04-04 14:50:21 +0000

    add true function to find number of bytes matching a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strspn - function to get length of prefix substring
+ * _strspn - function to get number of bytes of string in substring
  * @s: the string
  * @accept: the bytes to select from
  *
@@ -9,14 +9,32 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int i, j, k, len_of_accept;
+
+	j = 0;
+	while (*(accept + j) != '\0')
+		j++;
+	len_of_accept = j;
 
 	i = 0;
-	while (s[i] != *accept)
+	while (1)
 	{
-		i++;
+		if (s[i] == *accept)
+		{
+			i++;
+		}
+		else
+		{
+			for (k = 1; k < len_of_accept; k++)
+			{
+				if (s[i] == *(accept + k))
+				{
+					i++;
+					break;
+				}
+			}
+			if (k == len_of_accept)
+				return (i * sizeof(char));
+		}
 	}
-	i++;
-
-	return (i * sizeof(char));
 }
