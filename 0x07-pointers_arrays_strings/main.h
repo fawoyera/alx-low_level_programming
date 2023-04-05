69d909c2e6c51a0b9e4f20ce22b0c20b76a84216
fawoyera <femiawoyera@gmail.com>
2023-04-05 14:32:52 +0000

    print chess board

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 #define NULL ((void *)0)
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
+void _putchar(char c);
 
 #endif /* _MAIN_H_ */
