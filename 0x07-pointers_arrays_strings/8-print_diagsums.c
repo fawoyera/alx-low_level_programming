32e86dd8bed0e3f6cf848b2eda18a03e75e09300
fawoyera <femiawoyera@gmail.com>
2023-04-05 16:26:50 +0000

    print sum of two diagonals of square matrix of integers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagsums - function to print sum of two diagonals of square matrix
+ * @a: the matrix
+ * @size: size of the matrix
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, temp;
+	unsigned int left_diag_sum, right_diag_sum;
+
+	left_diag_sum = 0;
+	right_diag_sum = 0;
+	temp = size - 1;
+	for (i = 0; i < size; i++)
+	{
+		left_diag_sum += *(a + (i * (size + 1)));
+		right_diag_sum += *(a + temp);
+		temp += size - 1;
+	}
+	printf("%u, %u\n", left_diag_sum, right_diag_sum);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
 #include <unistd.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +11,8 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void _putchar(char c);
+void print_diagsums(int *a, int size);
+
+
 
 #endif /* _MAIN_H_ */
