65b2570905396121a1cd5235fa0165c42a19534d
fawoyera <femiawoyera@gmail.com>
2023-04-08 05:05:32 +0000

    compare two strings

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,7 @@ int is_prime_number(int n);
 int _is_prime(int n, int i);
 int is_palindrome(char *s);
 int is_pal(char *s, int i, int len);
+int wildcmp(char *s1, char *s2);
+
 
 #endif /* _MAIN_H_ */
