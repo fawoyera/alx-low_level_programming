ed2ff1fb15c4051aecfe1575cf776658a8a6580c
fawoyera <femiawoyera@gmail.com>
2023-04-08 02:27:21 +0000

    check if a string is a palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,7 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 int is_prime_number(int n);
 int _is_prime(int n, int i);
-
+int is_palindrome(char *s);
+int is_pal(char *s, int i, int len);
 
 #endif /* _MAIN_H_ */
