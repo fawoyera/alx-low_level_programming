630bc04b73927a704b283561266947fd7f5d3699
fawoyera <femiawoyera@gmail.com>
2023-04-10 07:09:28 -0700

    add positive numbers passed from the command line

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 if Success
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc - 1 < 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
