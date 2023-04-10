f1bdbfc538597a721c4d210c8a5a988c300a3192
fawoyera <femiawoyera@gmail.com>
2023-04-10 06:13:08 -0700

    print the number of command line arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 for Success
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
