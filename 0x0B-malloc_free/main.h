e890e25ecda50d02bd62e14bbf203270182b651e
fawoyera <femiawoyera@gmail.com>
2023-04-11 01:22:25 -0700

    return pointer to newly allocated memory space with a copy of string given as parameter

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+#include <stdlib.h>
 void _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+
 
 #endif /* _MAIN_H_ */
