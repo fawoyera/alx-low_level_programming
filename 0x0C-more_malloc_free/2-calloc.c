137c0dccad43718c9db825596762fe88cdd20d52
fawoyera <femiawoyera@gmail.com>
2023-04-23 17:51:52 +0000

    allocate memory for an array and set to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,12 +7,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int i;
+	unsigned int *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr == 0)
-		return (NULL);
+		return (0);
+	for (i = 0; i < nmemb * size; i++)
+		((char *)ptr)[i] = 0;
 	return (ptr);
 }
