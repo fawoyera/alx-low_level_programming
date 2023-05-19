3eb3117910f1d0d3909d3818b67a27da73ca3408
fawoyera <femiawoyera@gmail.com>
2023-05-19 03:22:37 -0700

    print with format specifiers d and i included

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
+int _print_num(int, int);
 /**
  * _printf - prints formatted arguments passed
  * @format: pointer to format specifiers
- *
  * Return: If Success- number of characters printed to stdout otherwise -1
  */
 int _printf(const char *format, ...)
@@ -31,6 +31,12 @@ int _printf(const char *format, ...)
 			while (*string)
 				_putchar(*string++), count++;
 			break;
+		case 'd':
+			count = _print_num(va_arg(ap, int), count);
+			break;
+		case 'i':
+			count = _print_num(va_arg(ap, int), count);
+			break;
 		default:
 			_putchar(format[i]), count++;
 		}
@@ -38,5 +44,39 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(ap);
+	return (count);
+}
+
+/**
+ * _print_num - print a decimal number to stdout
+ * @num: the number to print
+ * @count: number of characters printed
+ *
+ * Return: void
+ */
+int _print_num(int num, int count)
+{
+	int j = 0, temp, neg = 0;
+	char *tmp_str;
+
+	temp = num;
+	if (temp < 0)
+		num = temp =  0 - temp, neg = 1;
+	while (temp > 9)
+		temp /= 10, ++j;
+	++j;
+	tmp_str = malloc((j + 1) * sizeof(char));
+	if (tmp_str == NULL)
+		exit(-1);
+	tmp_str[j] = '\0';
+	while (j)
+	{
+		tmp_str[--j] = (num % 10) + '0';
+		num /= 10;
+	}
+	if (neg == 1)
+		_putchar('-'), count++;
+	while (*tmp_str)
+		_putchar(*tmp_str++), count++;
 	return (count);
 }
--- printf/main.h
+++ printf/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
 int _printf(const char *format, ...);
