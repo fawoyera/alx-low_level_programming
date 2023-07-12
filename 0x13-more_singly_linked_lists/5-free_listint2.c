4a6386453cb318ff4b81f403d66e8e2aaa7d9d60
fawoyera <femiawoyera@gmail.com>
2023-07-12 04:44:18 -0700

    free a listint_t list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
 	if (head == NULL)
-	{
 		return;
-	}
-	while (temp != NULL)
+
+	while (*head)
 	{
-		*head = temp->next;
-		free(temp);
-		temp = *head;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+
+	*head = NULL;
 }
