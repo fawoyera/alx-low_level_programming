41ffd7cd782f7a9e74571b02f7d46545898ac765
fawoyera <femiawoyera@gmail.com>
2023-07-29 10:23:36 -0700

    copy the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,13 +13,13 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_r = open(av[1], O_RDONLY);
 	if (fd_r == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	fd_w = open(av[2], O_CREAT | O_RDWR | O_TRUNC,
+	fd_w = open(av[2], O_CREAT | O_WRONLY | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_w == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
