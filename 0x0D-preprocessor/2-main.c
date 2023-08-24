9080db1767aa518326b8b330de90aa23f50c77c1
fawoyera <femiawoyera@gmail.com>
2023-08-24 03:23:16 -0700

    preprocessor projects update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print current filename
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Pre-processor directives and Macros in C
