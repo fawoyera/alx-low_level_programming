3b10dce1da89c6a77a7458d4b96bf2f5862e8487
fawoyera <femiawoyera@gmail.com>
2023-08-24 03:26:15 -0700

    singly linked lists project updates

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - prints a string before executing main function
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
