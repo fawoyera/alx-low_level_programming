f1366c38ccd6d6d29de811b3e3e3f2e24ea81e84
fawoyera <femiawoyera@gmail.com>
2023-11-02 19:42:47 +0000

    allocate memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - allocate memory using malloc and exit if unsuccessful
+ * @b: amount of bytes of memory to allocate
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == 0)
+		exit(98);
+	return (ptr);
+}
